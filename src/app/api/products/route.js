dab582a76d662fe8c26e786a8aae95050ae74d7f
Mr-ASbaloch <abdulsaeed1356@gmail.com>
2023-11-18 13:10:03 +0500

    commit

--- src/app/api/products/route.js
+++ src/app/api/products/route.js
@@ -1,3 +1,19 @@
+// import { NextResponse } from "next/server";
+// import mongoose from "mongoose";
+// import { connectionStr } from "@/lib/db";
+// import { product } from "@/lib/model/product";
+
+// export async function GET(request) {
+//   try {
+//     //
+//     await mongoose.connect(connectionStr);
+//     const data = product.find();
+//     console.log(data);
+
+//     return NextResponse.json("hello");
+//   } catch {}
+// }
+
 import { NextResponse } from "next/server";
 import mongoose from "mongoose";
 import { connectionStr } from "@/lib/db";
@@ -5,11 +21,19 @@ import { product } from "@/lib/model/product";
 
 export async function GET(request) {
   try {
-    //
     await mongoose.connect(connectionStr);
-    const data = product.find();
+    
+    // Corrected: Added await and exec() to get the result of the query
+    const data = await product.find().exec();
+    
     console.log(data);
 
-    return NextResponse.json("hello");
-  } catch {}
+    // Corrected: Closing the MongoDB connection
+    await mongoose.disconnect();
+
+    return NextResponse.json(data);
+  } catch (error) {
+    console.error(error);
+    return NextResponse.error("Internal Server Error", 500);
+  }
 }
